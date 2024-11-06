7c6b0093a811e69a5ed9584de83682136a3f5cc2
Cortez911 <morozovsergei.frost@gmail.com>
2024-11-06 23:17:58 +0300

    commit_6.1

--- Lesson_6/loading_images_dz6.py
+++ Lesson_6/loading_images_dz6.py
@@ -9,19 +9,8 @@ driver = webdriver.Chrome()
 # Открываем сайт
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-# Ждем, пока все изображения загрузятся
-WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.TAG_NAME, "img")))
+waiter = WebDriverWait(driver, 30)
 
-# Находим все изображения
-images = driver.find_elements(By.TAG_NAME, "img")
+waiter.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#award")))
 
-# Проверяем, есть ли на странице изображения
-if len(images) >= 3:
-  # Получаем значение атрибута src у 3-го изображения
-  third_image = images[2]
-  src_value = third_image.get_attribute("src")
-  # Выводим значение в консоль
-  print(src_value)
-
-# Закрываем браузер
-driver.quit()
+print(driver.find_element(By.CSS_SELECTOR, "#award").get_dom_attribute("src"))
