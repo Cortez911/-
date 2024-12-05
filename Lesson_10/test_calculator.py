65eaa8af3735370973dbc2c84a0d94c2652a5a2a
Cortez911 <morozovsergei.frost@gmail.com>
2024-12-06 00:05:24 +0300

    push_10_1_2

--- Lesson_10/test_calculator.py
+++ Lesson_10/test_calculator.py
@@ -5,22 +5,24 @@ from calculator_page import SlowCalculatorPage
 
 
 @allure.title("Тест калькулятора")
-@allure.description("Проверяет работу медленного калькулятора.")
+@allure.description("Проверяет работу калькулятора.")
 @allure.feature("Калькулятор")
 @allure.severity(allure.severity_level.CRITICAL)
 def test_slow_calculator(browser: webdriver.Chrome):
     calculator_page = SlowCalculatorPage(browser)
-    calculator_page.open()
-    calculator_page.set_delay("45")
-    calculator_page.click_button("7")
-    calculator_page.click_button("+")
-    calculator_page.click_button("8")
-    calculator_page.click_button("=")
-    def get_result(self) -> str:
-        try:
-            result_element = self.wait.until(EC.presence_of_element_located(self.result_locator))
-            # Ожидание, пока текст изменится и станет непустым
-            self.wait.until(lambda driver: result_element.text.strip() and result_element.text.strip().isdigit())
-            return result_element.text.strip()
-        except (TimeoutException, NoSuchElementException) as e:
-            return f"Ошибка получения результата: {e}"
+    with allure.step("Открытие страницы калькулятора"):
+        calculator_page.open()
+    with allure.step("Установка задержки"):
+        calculator_page.set_delay("45")
+    with allure.step("Ввод числа 7"):
+        calculator_page.click_button("7")
+    with allure.step("Выбор операции сложения"):
+        calculator_page.click_button("+")
+    with allure.step("Ввод числа 8"):
+        calculator_page.click_button("8")
+    with allure.step("Нажатие кнопки равно"):
+        calculator_page.click_button("=")
+    with allure.step("Проверка результата"):
+        result = calculator_page.get_result()
+        if result == "15":
+            assert True
